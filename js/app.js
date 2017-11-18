2bfc3790042d5225f069e0bc919c6e91c364c583
Lindsay M <lminami23@punahou.edu>
2017-11-18 10:16:01 -1000

    All Done!

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var btn5 = document.getElementById('btn5');
+var btn6 = document.getElementById('btn6');
+var btn7 = document.getElementById('btn7');
+var btn8 = document.getElementById('btn8');
+var btn9 = document.getElementById('btn9');
+
+btn1.addEventListener('click', function(){
+	//Favorite Place to Travel to
+	btn1.innerHTML = 'Japan';
+})
+btn2.addEventListener('click', function(){
+	//Favorite Color
+	btn2.innerHTML = 'Blue';
+})
+btn3.addEventListener('click', function(){
+	//Favorite Food
+	btn3.innerHTML = 'Cake';
+})
+btn4.addEventListener('click', function(){
+	//Favorite Song ATM
+	btn4.innerHTML = 'Good Old Days- Macklemore (feat. Kesha)';
+})
+btn5.addEventListener('click', function(){
+	//Favorite Ice Cream Flavor
+	btn5.innerHTML = 'Cookies and Cream';
+})
+btn6.addEventListener('click', function(){
+	//Favorite Anime
+	btn6.innerHTML = 'Tokyo Ghoul/Assination Classroom';
+})
+btn7.addEventListener('click', function(){
+	//Favorite Phone Game
+	btn7.innerHTML = 'Voez';
+})
+btn8.addEventListener('click', function(){
+	//Favorite Animal
+	btn8.innerHTML = 'Panda';
+})
+btn9.addEventListener('click', function(){
+	//Favorite TV Show
+	btn9.innerHTML = 'Riverdale';
+})
